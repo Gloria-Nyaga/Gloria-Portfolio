b4af26256093cd47e9398885908f9300beb6cde1
Gloria-Nyaga <nyagagakiigloria@gmail.com>
2024-11-18 15:59:44 +0300

    made chnages

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import TypewriterEffect from '../TypewriterEffect';
-import './index.css'; 
+import './index.css';
 import { FaDownload } from "react-icons/fa";
 import { FaFacebookF } from "react-icons/fa";
 import { TbBrandGithubFilled } from "react-icons/tb";
@@ -10,24 +10,22 @@ import { FaTwitter } from "react-icons/fa";
 function LandingPage() {
   return (
     <main className="landing-page">
-       <div className="text-section">
+      <div className="text-section">
         <h1 className="heading">Hi, I'm Gloria Nyaga</h1>
-        <h3>And I'm a<TypewriterEffect text="Sooftware Developer" className="typewriter-effect" /></h3>
-        
+        <h3>And I'm a <TypewriterEffect text="Software Developer" className="typewriter-effect" /></h3>
+
         <p className="description">
           I believe that the intersection of software development and UI/UX design inspires my passion to develop 
           seamless experiences that prioritize user needs. By blending technical expertise with my design skills, 
-          my goal is to create engaging interfaces that enhance interaction
-          and foster connection.
-
+          my goal is to create engaging interfaces that enhance interaction and foster connection.
         </p>
-        <div className="btn-box">
-          <a href="#">Download CV &nbsp;<FaDownload /></a> 
-          <a href="#">Hire Me</a>
-        </div>
 
-        <span class="home-imgHover"></span>
+        <div className="btn-box">
+          <a href="#" className="btn-download">Download CV &nbsp;<FaDownload /></a>
+          <a href="#" className="btn-hire">Hire Me</a>
+        </div>
       </div>
+
       <div className="image-section">
         <img 
           src="/images/gloria.png" 
@@ -38,22 +36,10 @@ function LandingPage() {
       </div>
 
       <div className="social-links">
-
-        <a href="https://github.com/Gloria-Nyaga" className="github">
-        <TbBrandGithubFilled />
-        </a>
-
-        <a href="https://www.linkedin.com/in/nyaga-gloria-683a292b2" className="linkedin">
-        <FaLinkedinIn />
-        </a>
-
-        <a href="https://x.com/nyaga4gloria" className="twitter">
-        <FaTwitter />
-        </a>
-
-        <a href="#" className="behance">
-        <FaFacebookF />
-        </a>
+        <a href="https://github.com/Gloria-Nyaga" className="github"><TbBrandGithubFilled /></a>
+        <a href="https://www.linkedin.com/in/nyaga-gloria-683a292b2" className="linkedin"><FaLinkedinIn /></a>
+        <a href="https://x.com/nyaga4gloria" className="twitter"><FaTwitter /></a>
+        <a href="#" className="facebook"><FaFacebookF /></a>
       </div>
     </main>
   );
